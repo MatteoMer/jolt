a18eb69b203efee149d119318e75e38af593aca3
MatteoMer <30910760+MatteoMer@users.noreply.github.com>
2024-07-20 16:43:54 +0200

    GKR not working

--- jolt-evm-verifier/test/GrandProductArgumentGasWrapper.sol
+++ jolt-evm-verifier/test/GrandProductArgumentGasWrapper.sol
@@ -15,20 +15,6 @@ error SumcheckFailed();
 
 contract GrandProductArgumentGasWrapper is TestBase {
     using FiatShamirTranscript for Transcript;
-    bool private enableLogging = vm.envBool("BENCHMARK_LOGGING");
-
-    function conditionalLog(string memory message) internal view {
-        if (enableLogging) {
-            console.log(message);
-        }
-    }
-
-    function conditionalLog(string memory message, uint256 value) internal view {
-        if (enableLogging) {
-            console.log(message, value);
-        }
-    }
-
 
     function verifySumcheckLayer(
         Jolt.BatchedGrandProductLayerProof memory layer,
@@ -36,12 +22,12 @@ contract GrandProductArgumentGasWrapper is TestBase {
         Fr claim,
         uint256 degree_bound,
         uint256 num_rounds
-    ) public pure returns (Fr, Fr[] memory) {
+    ) external view returns (Fr, Fr[] memory) {
         return GrandProductArgument.verifySumcheckLayer(layer, transcript, claim, degree_bound, num_rounds);
     }
 
 
-    function buildEqEval(Fr[] memory rGrandProduct, Fr[] memory rSumcheck) public pure returns (Fr eqEval) {
+    function buildEqEval(Fr[] memory rGrandProduct, Fr[] memory rSumcheck) external view returns (Fr eqEval) {
         return GrandProductArgument.buildEqEval(rGrandProduct, rSumcheck);
     }
 
@@ -55,29 +41,27 @@ contract GrandProductArgumentGasWrapper is TestBase {
         Fr[] memory claims,
         Fr[] memory rGrandProduct,
         Transcript memory transcript
-    ) public view returns (Fr[] memory newClaims, Fr[] memory newRGrandProduct) {
+    ) external view returns (Fr[] memory newClaims, Fr[] memory newRGrandProduct) {
         return GrandProductArgument.verifySumcheckClaim(layerProofs, layerIndex, coeffs, sumcheckClaim, eqEval, claims, rGrandProduct, transcript);
     }
 
+
+    /* Here I'm doing the same thing as in the library, 
+     * but wrapping the internal functions to external ones so they appears in foundry's gas-report */
     function verify(
         Jolt.BatchedGrandProductProof memory proof,
         Fr[] memory claims,
         Transcript memory transcript
     ) external view returns (Fr[] memory) {
         Fr[] memory rGrandProduct = new Fr[](0);
-        uint gasUsed = gasleft();
 
         for (uint256 i = 0; i < proof.layers.length; i++) {
-            conditionalLog("[grand-product] round:", i);
-            //get coeffs
             uint256[] memory loaded = transcript.challenge_scalars(claims.length, MODULUS);
             Fr[] memory coeffs;
-            // TODO - hard convert should be removed when the transcript gets better Fr native typed support.
             assembly {
                 coeffs := loaded
             }
 
-            //create a joined claim
             Fr joined_claim = Fr.wrap(0);
             for (uint256 k = 0; k < claims.length; k++) {
                 joined_claim = joined_claim + (claims[k] * coeffs[k]);
@@ -90,38 +74,49 @@ contract GrandProductArgumentGasWrapper is TestBase {
                 revert GrandProductArgumentFailed();
             }
 
-            gasUsed = gasleft();
-            // verify sumcheck and get rSumcheck
+            // What's commented below works 
+            /* (Fr sumcheckClaim, Fr[] memory rSumcheck) =
+                verifySumcheckLayer(proof.layers[i], transcript, joined_claim, 3, i); */
+
+            /* But the code below crashes because of a Sumcheck claim mismatch.
+             *   
+             * I'm using `this.verifySumcheckLayer` to use it as an external function
+             * I also tried to make the wrapper functions fully external and call the verify code in the test directly 
+             * and got same results */
             (Fr sumcheckClaim, Fr[] memory rSumcheck) =
-                verifySumcheckLayer(proof.layers[i], transcript, joined_claim, 3, i);
-            gasUsed -= gasleft();
-            conditionalLog("[grand-product] verifySumcheckLayer gas usage:", gasUsed);
+                this.verifySumcheckLayer(proof.layers[i], transcript, joined_claim, 3, i);
 
             if (rSumcheck.length != rGrandProduct.length) {
                 revert GrandProductArgumentFailed();
             }
 
-            // Append the right and left claims to the transcript
             for (uint256 l = 0; l < proof.layers[l].leftClaims.length; l++) {
                 transcript.append_scalar(Fr.unwrap(proof.layers[i].leftClaims[l]));
                 transcript.append_scalar(Fr.unwrap(proof.layers[i].rightClaims[l]));
             }
 
-            gasUsed = gasleft();
-            Fr eqEval = buildEqEval(rGrandProduct, rSumcheck);
-            gasUsed -= gasleft();
-            conditionalLog("[grand-product] buildEqEval gas usage:", gasUsed);
+            // works
+            /* Fr eqEval = buildEqEval(rGrandProduct, rSumcheck); */
+
+            // crashes
+            Fr eqEval = this.buildEqEval(rGrandProduct, rSumcheck);
 
             rGrandProduct = new Fr[](rSumcheck.length);
             for (uint256 l = 0; l < rSumcheck.length; l++) {
                 rGrandProduct[l] = rSumcheck[rSumcheck.length - 1 - l];
             }
 
-            gasUsed = gasleft();
+
+            // works
+            /* (claims, rGrandProduct) =
+                verifySumcheckClaim(proof.layers, i, coeffs, sumcheckClaim, eqEval, claims, rGrandProduct, transcript); */
+
+            // crashes
             (claims, rGrandProduct) =
-                verifySumcheckClaim(proof.layers, i, coeffs, sumcheckClaim, eqEval, claims, rGrandProduct, transcript);
-            gasUsed -= gasleft();
-            conditionalLog("[grand-product] verifySumcheckClaim gas usage:", gasUsed);
+                this.verifySumcheckClaim(proof.layers, i, coeffs, sumcheckClaim, eqEval, claims, rGrandProduct, transcript);
+
+            /* From my investigations, it seems that the coeffs (that I'm getting via the fiat-shamir lib) are not valid when
+             * using external functions ? */
         }
 
         return rGrandProduct;
